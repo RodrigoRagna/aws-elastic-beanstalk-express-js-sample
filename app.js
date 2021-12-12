539a784dedb3e064952f09b8159b435beebec54c
Rodrigo <rodrigo.rivera@ciecnias.unam.mx>
2021-12-12 00:26:59 -0600

    test request

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World! in aws'));
 
+app.post('/test', (req, res) => {
+    let r = Math.floor(Math.random() * 10)
+    if (r <= 5) {
+        res.status(409).send({ mensaje: "Error" });
+    } else {
+
+        res.status(200).send({ mensaje: "Exito" })
+    }
+}
+);
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
