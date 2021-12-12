b0f4eca80dffbfd42570d4fe68d968db4a7425d1
Rodrigo <rodrigo.rivera@ciecnias.unam.mx>
2021-12-12 00:41:04 -0600

    logs

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! in aws'));
+app.get('/', (req, res) => {
+    console.log("Mi home !")
+
+    res.send('Hello World! in aws')
+});
 
 app.post('/test', (req, res) => {
     let r = Math.floor(Math.random() * 10)
+    console.log("valor :", r)
     if (r <= 5) {
         res.status(409).send({ mensaje: "Error" });
     } else {
